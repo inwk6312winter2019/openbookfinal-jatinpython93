c5f948f93dafaf0607cb9e481fe4619a6c80ae0b
jatinpython93 <Roshna.LakshmiChandrasekaran@dal.ca>
2019-02-15 11:10:22 -0800

    access file read and four log files created

---
+++ task2.py
@@ -0,0 +1,14 @@
+fout = open("access.log" , "r")			# Opening the accessing log file
+
+
+log1_file = open("Get.txt" , "w") 		# Get file created 
+
+log2_file = open("Post.txt" , "w")		# Post file created
+
+log3_file = open("put.txt" , "w")		# Put file created
+
+log4_fle = open("Delete.txt" , "w")		# Delete file created 
+
+
+
+
